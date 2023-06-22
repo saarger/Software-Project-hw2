f8ebec47daafe70af9ac1d7c8420620991ede6f6
saar.gerassi <saar.gerassi@digitalturbine.com>
2023-06-22 17:37:26 +0300

    add bonus

--- bonus.py
+++ bonus.py
@@ -6,14 +6,14 @@ import numpy as np
 
 def main():
     data = datasets.load_iris().data
-    Ks = [i for i in range(1, 11)]
+    EM = [i for i in range(1, 11)]
     y = [np.sum(np.sum((data - np.mean(data, axis=0))**2))]
 
-    for k in Ks[1:]:
+    for k in EM[1:]:
         kmeans = cluster.k_means(data, n_clusters=k, random_state=0, init='k-means++')
         y.append(kmeans[2])
 
-    plt.plot(Ks, y, color='purple')
+    plt.plot(EM, y, color='purple')
     plt.xticks(np.arange(1, 11))
     plt.xlabel("K")
     plt.ylabel("Inertia")
@@ -24,6 +24,8 @@ def main():
                                  fc="w"), )
     plt.plot(2, 150, 'o', ms=20, mec='black', mfc='none', mew=2, linestyle="--")
     plt.savefig("elbow.png")
+
+
 
 
 if __name__ == '__main__':
